1d7feed6b6812588d7b14c7b6d18f011502a13f4
Michele Copelli <michele.copelli@engagelabs.it>
2023-09-30 16:28:15 +0200

    adds base exception and result object

---
+++ src/Domain/Exceptions/DomainExceptions.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Domain.Exceptions;
+
+public abstract class DomainExceptions : Exception
+{
+    protected DomainExceptions() : base()
+    {
+
+    }
+
+    protected DomainExceptions(string message) : base(message)
+    {
+    }
+
+    protected DomainExceptions(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+
+    }
+
+    public DomainExceptions(string? message, Exception? innerException) : base(message, innerException)
+    {
+
+    }
+}
---
+++ src/Domain/Shared/Error.cs
@@ -0,0 +1,70 @@
+﻿namespace Domain.Shared;
+
+public class Error : IEquatable<Error>
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "Result value is null");
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+    public string Message { get; }
+
+    public static implicit operator string(Error error) => error.Code;
+
+    public static bool operator ==(Error left, Error right)
+    {
+        if (left is null && right is null)
+        {
+            return true;
+        }
+
+        if (left is null || right is null)
+        {
+            return false;
+        }
+
+        return left.Code == right.Code;
+    }
+
+    public static bool operator !=(Error left, Error right) => !(left == right);
+
+    bool IEquatable<Error>.Equals(Error? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        return Code == other.Code;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (obj is not Error error)
+        {
+            return false;
+        }
+
+        return error.Code == Code;
+    }
+
+    public override int GetHashCode()
+    {
+        return Code.GetHashCode() * 42;
+    }
+}
