443567fdc13e60d3ee37171224cbb6899abcc745
Michele Copelli <michele.copelli@engagelabs.it>
2023-10-01 15:05:20 +0200

    adds CQRS abstractions

---
+++ src/Presentation/Abstractions/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Presentation.Abstractions;
+
+public abstract class ApiController : ControllerBase
+{
+    protected readonly ISender Sender;
+
+    protected ApiController(ISender sender)
+    {
+        Sender = sender;
+    }
+}
